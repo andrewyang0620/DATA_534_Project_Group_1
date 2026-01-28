e795aa0f0a483be5b42b65d6e3ccdbc5c573f1c1
andrewyang0620 <yangjingtaoitb@gmail.com>
2026-01-27 16:06:31 -0800

    utils_api_R added

---
+++ R/utils_api.R
@@ -0,0 +1,20 @@
+#' @keywords internal
+bc_base_url <- function() {
+  "https://www.bclaws.ca/civix"
+}
+
+#' Low-level helper to call BC Laws API
+#' @keywords internal
+bc_get <- function(path, query = list()) {
+  stopifnot(is.character(path), length(path) == 1)
+
+  path <- sub("^/+", "", path)
+
+  req <- httr2::request("https://www.bclaws.ca/civix") |>
+    httr2::req_url_path_append(path) |>
+    httr2::req_url_query(!!!query)
+
+  resp <- httr2::req_perform(req)
+  httr2::resp_check_status(resp)
+  resp
+}
