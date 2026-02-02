d58ea03fc4a1d720be36666fd7173e54813e1ccd
2004wan <wyr666@student.ubc.ca>
2026-02-02 11:00:19 -0800

    test cases added

--- tests/testhat/test-parse_search_results.R
+++ tests/testhat/test-parse_search_results.R
@@ -1,1 +1,7 @@
-# sample test
+test_that("parse_search_results returns empty df when no <doc>", {
+  xml <- xml2::read_xml("<root></root>")
+  out <- parse_search_results(xml)
+  expect_s3_class(out, "data.frame")
+  expect_true(all(c("title","law_type","law_id","loc","hits") %in% names(out)))
+  expect_equal(nrow(out), 0)
+})
